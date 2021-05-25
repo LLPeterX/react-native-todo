c29c9fad4c746b2901e0e13b24b4a82e277fb3a6
Peter Lebedev <LPeter@mail.ru>
2021-05-25 15:51:21 +0300

    Alert

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
-import {THEME} from '../theme';
+import { StyleSheet, View, Text, Button, Alert } from 'react-native';
+import { THEME } from '../theme';
 
 export default function TodoScreen({ todo, onBack, onDelete }) {
   const removeTodo = () => {
-    onDelete(todo.id);
-    onBack();
+    Alert.alert('Удаление', // header
+    'Вы желаете удалить эту задачу?', // text
+      // buttons Yes/No
+      [
+        {
+          text: 'Да',
+          onPress: () => {
+            onDelete(todo.id);
+            onBack();
+          }
+        },
+        {
+          text: 'Нет'
+        }
+      ],
+      // options
+      {
+        cancelable: true
+      }
+    );
+    // onDelete(todo.id);
+    // onBack();
   }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>{todo.text}</Text>
       <View style={styles.block}>
-        <View style={styles.button}><Button title="< Назад" color={THEME.GRAY_COLOR} onPress={onBack}/></View>
+        <View style={styles.button}><Button title="< Назад" color={THEME.GRAY_COLOR} onPress={onBack} /></View>
         <View style={styles.button}><Button title="Изм." color={THEME.GREEN_COLOR} /></View>
-        <View style={styles.button}><Button title=" X " color={THEME.DANGER_COLOR} onPress={removeTodo}/></View>
+        <View style={styles.button}><Button title=" X " color={THEME.DANGER_COLOR} onPress={removeTodo} /></View>
       </View>
     </View>
   );
