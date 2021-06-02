fe1b3c115e02f16608d97731a214550d677de5f7
Peter Lebedev <LPeter@mail.ru>
2021-06-02 13:54:48 +0300

    apply todoContext to MainScreen in MainLayout

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,25 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { StyleSheet, View } from 'react-native';
 import NavBar from './components/Navbar';
+import { TodoContext } from './context/todo/todoContext';
 import MainScreen from './screens/MainScreen'
 import TodoScreen from './screens/TodoScreen'
 
 
 export const MainLayout = () => {
+  const todoContext = useContext(TodoContext);
   const [todoId, setTodoId] = useState(null); // state for current todo item
   const [todos, setTodos] = useState([]); // state of array of todos
   
   // handler to add todo into array
   const addTodo = (text) => {
     const newTodo = {
-      id: Date.now().toString(), // must be string (used as key value)
+      id: Date.now().toString(), // должна быть строка
       text
     };
-    //setTodos((prevTodos) => [...prevTodos, newTodo]);
     setTodos([...todos, newTodo ]);
   };
-
-  
   //handler to remove task by id
   const deleteTodo = (id) => setTodos((prevTodos) => prevTodos.filter(item => item.id != id));
 
@@ -36,7 +35,8 @@ export const MainLayout = () => {
   }
   
   // content of current screen
-  let screenContent = <MainScreen todos={todos} addTodo={addTodo} deleteTodo={deleteTodo} openTodo={setTodoId}/>;
+  let screenContent = <MainScreen 
+  todos={todoContext.todos} addTodo={addTodo} deleteTodo={deleteTodo} openTodo={setTodoId}/>;
   if(todoId) {
     let selectedTodo = todos.find(item =>item.id === todoId);
     if(selectedTodo) screenContent = <TodoScreen todo={selectedTodo} 
